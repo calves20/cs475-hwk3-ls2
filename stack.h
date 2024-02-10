a10365d94bd8bc753f90f777da722f9f06825763
calves20 <157167670+calves20@users.noreply.github.com>
2024-02-09 21:46:28 -0800

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,26 +1,23 @@
 #ifndef STACK_H_INCLUDED
 #define STACK_H_INCLUDED
-
-/**
- * Declaration of a stacknode_t type
- */
+// Authur @Curtis Alves
+// Defines the node structure for the stack
 typedef struct stacknode_t {
-  char* data;
-  struct stacknode_t *next;
+    char* data; // Holds a pointer to the string data
+    struct stacknode_t* next; // Points to the next node in the stack
 } stacknode_t;
 
-/**
- * Declaration of a stack_t type
- */
+// Defines the structure for the stack itself
 typedef struct stack_t {
-  stacknode_t *top;
-  int size;
+    stacknode_t* top; // Points to the top node in the stack
+    int size; // Tracks the number of items in the stack
 } stack_t;
 
-stack_t *initstack();
-void* pop(stack_t* s);
-void push(stack_t* s, void* item);
-void freestack(stack_t* s);
-void printstack(stack_t* s);
+// Function prototypes
+stack_t* initStack(); 
+char* pop(stack_t* s); 
+void push(stack_t* s, char* item);
+void freeStack(stack_t* s); 
+void printStack(stack_t* s);
 
-#endif
+#endif // STACK_H_INCLUDED
